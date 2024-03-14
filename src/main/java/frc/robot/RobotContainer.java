654456fcf3a6fd092e0e0f811f4ec0ed15f75926
teggos <teggorocks@gmail.com>
2024-03-14 19:48:57 -0400

    new controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -18,6 +18,7 @@ import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -70,6 +71,14 @@ public class RobotContainer {
         () -> Math.pow(driverController.getRawAxis(2), 3),
         () -> speedChooser.getSelected(),
         ahrs
+      )
+    );
+
+    driverController.button(3).onTrue(
+      new InstantCommand(
+        () -> {
+          ahrs.reset();
+        }
       )
     );
   }
